ef8f45e5110b0329e2cd81832aee2536609345ee
Abhra00 <abhramondal9674@gmail.com>
2025-12-19 15:42:48 +0530

    [Treesitter] Config refactored for main branch

---
+++ nvim/lua/plugins/ts-comments.lua
@@ -0,0 +1,12 @@
+-- ================================================================================================
+-- TITLE : ts-comments.nvim
+-- ABOUT : Tiny plugin to enhance Neovim's native comments
+-- -- LINKS :
+--   > github :https://github.com/folke/ts-comments.nvim
+-- ================================================================================================
+return {
+  'folke/ts-comments.nvim',
+  opts = {},
+  event = 'VeryLazy',
+  enabled = true,
+}
