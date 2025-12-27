f4da1785b8573834b8ec2ad47f74e661591b2fc6
Abhra00 <abhramondal9674@gmail.com>
2025-12-27 17:08:48 +0530

    Sophisticated root dir detection

--- nvim/lua/utils/lualine-utils.lua
+++ nvim/lua/utils/lualine-utils.lua
@@ -26,20 +26,37 @@ function M.lualine.pretty_path(opts)
       return ''
     end
 
-    path = vim.fs.normalize(path)
-    local cwd = vim.fs.normalize(vim.fn.getcwd())
+    -- preserve original path for display
+    local norm_path = vim.fs.normalize(path)
 
-    if opts.relative == 'cwd' and path:find(cwd, 1, true) == 1 then
-      path = path:sub(#cwd + 2)
+    local root_utils = require 'utils.root'
+    local root = root_utils.get { normalize = true }
+    local cwd = root_utils.cwd()
+
+    -- windows case-insensitive safety
+    if vim.fn.has 'win32' == 1 then
+      norm_path = norm_path:lower()
+      root = root:lower()
+      cwd = cwd:lower()
     end
 
-    local sep = '/'
-    local parts = vim.split(path, '/')
+    -- make path relative
+    if opts.relative == 'cwd' and norm_path:find(cwd, 1, true) == 1 then
+      path = path:sub(#cwd + 2)
+    elseif opts.relative == 'root' and norm_path:find(root, 1, true) == 1 then
+      path = path:sub(#root + 2)
+    end
 
-    if opts.length > 0 and #parts > opts.length then
+    -- platform separator
+    local sep = package.config:sub(1, 1)
+    local parts = vim.split(path, '[\\/]')
+
+    -- shorten path
+    if opts.length ~= 0 and #parts > opts.length then
       parts = { parts[1], '…', unpack(parts, #parts - opts.length + 2, #parts) }
     end
 
+    -- filename highlighting
     if opts.modified_hl and vim.bo.modified then
       parts[#parts] = parts[#parts] .. opts.modified_sign
       parts[#parts] = M.lualine.format(self, parts[#parts], opts.modified_hl)
@@ -47,12 +64,14 @@ function M.lualine.pretty_path(opts)
       parts[#parts] = M.lualine.format(self, parts[#parts], opts.filename_hl)
     end
 
+    -- directory highlighting
     local dir = ''
     if #parts > 1 then
       dir = table.concat({ unpack(parts, 1, #parts - 1) }, sep)
       dir = M.lualine.format(self, dir .. sep, opts.directory_hl)
     end
 
+    -- readonly icon
     local readonly = ''
     if vim.bo.readonly then
       readonly = M.lualine.format(self, opts.readonly_icon, opts.modified_hl)
@@ -63,7 +82,7 @@ function M.lualine.pretty_path(opts)
 end
 
 -- Root dir
----@param opts? {cwd: boolean?, subdirectory: boolean?, parent: boolean?, other: boolean?, icon: string?, color: function?}
+---@param opts? {cwd:false, subdirectory: true, parent: true, other: true, icon?:string}
 function M.lualine.root_dir(opts)
   opts = vim.tbl_extend('force', {
     cwd = false,
@@ -76,12 +95,33 @@ function M.lualine.root_dir(opts)
     end,
   }, opts or {})
 
+  local function get()
+    local root_utils = require 'utils.root'
+    local cwd = root_utils.cwd()
+    local root = root_utils.get { normalize = true }
+    local name = vim.fs.basename(root)
+
+    if root == cwd then
+      -- root is cwd
+      return opts.cwd and name
+    elseif root:find(cwd, 1, true) == 1 then
+      -- root is subdirectory of cwd
+      return opts.subdirectory and name
+    elseif cwd:find(root, 1, true) == 1 then
+      -- root is parent directory of cwd
+      return opts.parent and name
+    else
+      -- root and cwd are not related
+      return opts.other and name
+    end
+  end
+
   return {
     function()
-      return opts.icon .. vim.fn.fnamemodify(vim.fn.getcwd(), ':t')
+      return (opts.icon and opts.icon .. ' ') .. get()
     end,
     cond = function()
-      return true
+      return type(get()) == 'string'
     end,
     color = opts.color,
   }
